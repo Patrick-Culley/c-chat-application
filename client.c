1db8103d2ee8f69fb118ad06d2299f0a6da22a3f
b0tchd <misplacedfolder@gmail.com>
2023-04-30 01:05:02 -0500

    added receiver handler for client

--- client.c
+++ client.c
@@ -1,35 +1,64 @@
-#include "serverhead.h"
+#include "clienthead.h"
 
-int main()
+int main(int argc, char *argv[])
 {   
+    if (argc < 4)
+    {
+        printRed("Pass in port number, user name, and password"); 
+        return 1; 
+    }
+    if (argc > 4)
+    {
+        printRed("Only pass in port number, user name, and password"); 
+        return 1; 
+    }
+
+    port_number = atoi(argv[1]); 
+    strcpy(username, argv[2]); 
+    strcpy(password, argv[3]);
+
+    // AUTHENTICATE PASS // 
+    if (strcmp(password, server_password) != 0)
+    {
+        printRed("Password incorrect, try again");
+        return 1; 
+    } 
+
+    // CREATE SOCKET //  
     char buffer[1028];
-    int valread; 
-
-    // Create socket 
+    int valread;
     int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-
     if (socket_fd < 0)
     {
-        printf("SOCKET ERROR: ");
+        perror("SOCKET ERROR: ");
+        return 1; 
     };
 
     struct sockaddr_in address; 
     address.sin_family = AF_INET; 
-    address.sin_port = htons(3001);
-    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port_number);
+    address.sin_addr.s_addr = inet_addr(ip_address); 
     
-    // Assign name to socket 
-    int bind_ip = bind(socket_fd, (struct sockaddr*)&address, sizeof(address));
-
-    // Connect 
+    // CONNECT TO REMOTE SERVER //  
     int connection = connect(socket_fd, (struct sockaddr*)&address, sizeof(address));
-
     if (connection < 0)
     {
         perror("CONNECTION ERROR ");    
+        return 1; 
     };
+
+    status = 0; 
+    printMagenta("~~~ You have entered The Chat Room ~~~\n");
+
+    pthread_t receiving_thread; 
+    int new_socket = socket_fd; 
+    int *new_sock;
+    new_socket = malloc(1); 
+    *new_sock = new_socket; 
+
+    pthread_create(&receiving_thread, NULL, receiver_handler, (void*) new_sock);
+
     int wr, re, pr; 
-    printf("Connected to Chat!\nType 'later' to quit.");
     while (1)
     {   
         fgets(buffer, sizeof(buffer), stdin);
@@ -46,3 +75,50 @@ int main()
 
     return 0; 
 };
+
+void *receiver_handler(void *socket)
+{
+    int sock = *(int*)socket; 
+    int read_size; 
+    char server_message[2000]; 
+    char processed_server_message[2000]; 
+
+    // RECEIVE MSG FROM CLIENT 
+    while ((read_size = recv(sock, server_message, 2000, 0)) > 0)
+    {
+        int ie = 0; 
+        for (int i = 0; i <= strlen(server_message); i++)
+        {
+            if (server_message[i] == '\n'){
+                server_message[i] == '\0';
+            }
+            processed_server_message[i] = server_message[i]; 
+            ie = i; 
+        }
+        printBlue(processed_server_message); 
+        printf("\n");
+        processed_server_message[ie] = '\n';
+    }
+    if (read_size == 0){
+        printYellow("Server disconnected\n");
+        exit(0); 
+    } else if (read_size == -1){
+        perror("recv failed");
+    }
+    status = 0; 
+    free(socket); 
+
+    return 0; 
+}
+
+void printRed(char *string) {
+    printf("\033[1;31m");
+    printf("%s", string);
+    printf("\033[0m");
+}
+
+void printBlue(char *string) {
+    printf("\033[0;36m");
+    printf("%s", string);
+    printf("\033[0m");
+}
